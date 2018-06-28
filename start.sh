0d2c12b9961424d4f8d1a656100d92856ae61944
mrschlei <mrschlei@umich.edu>
2018-06-28 12:33:52 -0400

    clean up

--- start.sh
+++ start.sh
@@ -8,7 +8,6 @@ ln -sf /dev/stderr /var/log/apache2/error_log
 
 # apache and virtual host secrets
 ln -sf /secrets/apache2/apache2.conf /etc/apache2/apache2.conf
-cat /secrets/apache2/apache2.conf
 ln -sf /secrets/apache2/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
 ln -sf /secrets/apache2/cosign.conf /etc/apache2/mods-available/cosign.conf
 
